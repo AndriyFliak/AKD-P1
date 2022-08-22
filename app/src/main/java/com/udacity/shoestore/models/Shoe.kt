e40f905d9f4ab69bdeaf58c0284d0691883cc2fb
AndriyFliak <andrey0066@gmail.com>
2022-08-22 16:54:55 +0100

    Additional comments

--- app/src/main/java/com/udacity/shoestore/models/Shoe.kt
+++ app/src/main/java/com/udacity/shoestore/models/Shoe.kt
@@ -8,5 +8,19 @@ import kotlinx.parcelize.Parcelize
 data class Shoe(
     var name: String, var size: Double, var company: String, var description: String,
     val images: List<String> = mutableListOf()
-) : Parcelable, BaseObservable()
+) : Parcelable, BaseObservable() {
+
+    /* Tried this solution for size 2-way binding as per
+    https://developer.android.com/topic/libraries/data-binding/two-way
+    and used android:text="@={shoe.size}" in layout but it didn't work. Error:
+    Cannot find a getter for <android.widget.EditText android:text> that accepts parameter type 'double'
+    @Bindable
+    fun getSize(): String {
+        return size.toString()
+    }
+    fun setSize(size: String) {
+        this.size = size.toDouble()
+    }
+*/
+}
 
